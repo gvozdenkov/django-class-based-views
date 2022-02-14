ccbc21a2a6aefc5df61f8254d6d9eedd7e8ec69c
arty <gvozdenkov@gmail.com>
2022-02-14 09:58:48 +0300

    fix css to select background message. Change root path with form to http://localhost:8000/

--- hello/urls.py
+++ hello/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('hello', views.MessageView.as_view(success_url='/thank-you'), name='hello_there'),
+    path('', views.MessageView.as_view(success_url='/thank-you'), name='hello_there'),
     path('thank-you', views.ThankyouView.as_view(), name='thankyou'),
     path('messages', views.MessageListView.as_view(), name='all_messages'),
     path('messages/<int:pk>', views.MessageDetailView.as_view(), name='detail_message')
